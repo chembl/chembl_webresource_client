ea9a6b05e3f0f4f90666ec4a3a8348683f381774
Michał Nowotka <mmmnow@gmail.com>
2017-04-21 21:49:55 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -21,7 +21,16 @@ setup(
     url='https://www.ebi.ac.uk/chembldb/index.php/ws',
     license='Apache Software License',
     packages=['chembl_webresource_client'],
-    long_description=open('README.rst').read(),
+    long_description="""
+    Documentation and repository: https://github.com/chembl/chembl_webresource_client.
+    This is the only official Python client library developed and supported by ChEMBL (https://www.ebi.ac.uk/chembl/) group.
+
+    The library helps accessing ChEMBL data and cheminformatics tools from Python. 
+    You don't need to know how to write SQL. 
+    You don't need to know how to interact with REST APIs. 
+    You don't need to compile or install any cheminformatics framework. 
+    Results are cached.
+    """,
     install_requires=[
         'six',
         'urllib3',
