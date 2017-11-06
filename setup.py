00699c8b015589396c312184c08f400e6d710ca8
Michal Nowotka <mnowotka@ebi.ac.uk>
2017-11-06 18:42:15 +0000

    new tests added

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl-webresource-client',
-    version='0.9.23',
+    version='0.9.24',
     author='Michal Nowotka',
     author_email='mnowotka@ebi.ac.uk',
     description='Python client fot accessing ChEMBL webservices.',
