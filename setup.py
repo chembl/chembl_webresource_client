191225c7941779438ea86359cc68c6b6839f0f25
Lejla Gul <Lejla.Gul@earlham.ac.uk>
2019-01-17 18:20:31 +0100

    Version update

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 setup(
     name='chembl-webresource-client',
-    version='0.9.31',
+    version='0.9.31patch1',
     entry_points={
         'console_scripts': [
             'chembl_ids=chembl_webresource_client.scripts.chembl_ids:main',
