1e6a16812e99924e5fff70f8e8a89243fc022e7d
Eloy Felix <eloyfelix@gmail.com>
2019-09-09 16:06:22 +0100

    gevent not used anymore

--- chembl_webresource_client/settings.py
+++ chembl_webresource_client/settings.py
@@ -16,7 +16,6 @@ class Settings:
     TOTAL_RETRIES = 3
     BACKOFF_FACTOR = 2
     CONCURRENT_SIZE = 50
-    ASYNC_TRESHOLD = 10
     CACHE_EXPIRE = 60 * 60 * 24
     CACHE_NAME = default_cache_name
     RESPECT_RATE_LIMIT = True
