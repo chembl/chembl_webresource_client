758e1e1f3dbb2d75905bddab90bbe16c6283e520
eloyfelix <eloyfelix@gmail.com>
2020-03-03 20:23:12 +0000

    fix #71

--- chembl_webresource_client/new_client.py
+++ chembl_webresource_client/new_client.py
@@ -55,7 +55,7 @@ def client_from_url(url, base_url=None):
             continue
         model = Model(name, collection_name, formats, searchable)
         qs = QuerySet(model=model)
-        if default_format != 'xml':
+        if default_format in ('xml', 'image/svg+xml'):
             qs.set_format(default_format)
         setattr(client, name, qs)
 
