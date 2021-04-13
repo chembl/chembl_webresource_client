0cec8abab63ce0a6b156fc2eb275e56e4a9aa00d
Eloy Félix <eloyfelix@gmail.com>
2021-04-13 12:44:52 +0100

    fix issue with requests-cache 0.6.0 (#100)

--- chembl_webresource_client/cache.py
+++ chembl_webresource_client/cache.py
@@ -1,15 +1,17 @@
 __author__ = 'mnowotka'
 
-from requests_cache.backends.base import BaseCache, hashlib, _to_bytes
+from requests_cache.backends.base import BaseCache
+from requests_cache.cache_keys import _encode
+import hashlib
 
-def create_key(self, request):
+def create_key(self, request, **kwargs):
     key = hashlib.sha256()
-    key.update(_to_bytes(request.method.upper()))
-    key.update(_to_bytes(request.url))
+    key.update(_encode(request.method.upper()))
+    key.update(_encode(request.url))
     if request.body:
-        key.update(_to_bytes(request.body))
+        key.update(_encode(request.body))
     if request.headers and 'Accept' in request.headers:
-        key.update(_to_bytes(request.headers['Accept']))
+        key.update(_encode(request.headers['Accept']))
     return key.hexdigest()
 
 def monkeypatch_requests_cache():
